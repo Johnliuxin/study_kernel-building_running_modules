04abdf0edcc6b641e2967405e3a9cf28a9352eca
John.LiuXin <liuxing@embest-tech.com>
2015-08-02 13:58:56 +0800

    module param: add example for module param
    example for module_param and module_param_array
    
    Signed-off-by: John.LiuXin <liuxing@embest-tech.com>

--- HelloWorld.c
+++ HelloWorld.c
@@ -8,15 +8,37 @@
  */
 MODULE_LICENSE("Dual BSD/GPL");
 
+/*
+ * Module parameter
+ */
+static char *whom = "Josh";
+module_param(whom, charp, S_IRUGO);
+
+static int how_many = 1;
+module_param(how_many, int, S_IRUGO);
+
+static int int_array[6] = {1, 2, 3, 4, 5, 6};
+static int int_array_num;
+module_param_array(int_array, int, &int_array_num, S_IRUGO);
+
 static int __init hello_init(void)
 {
-    printk(KERN_ALERT "Hello, world\n");
-    return 0;
+	int i;
+	for (i=0; i < how_many; i++) {
+		printk(KERN_ALERT "Hello %s\n", whom);
+	}
+
+	printk(KERN_ALERT "array element input from user: %d\n", int_array_num);
+
+	for (i=0; i < 6; i++) {
+		printk(KERN_ALERT "int_array[%d]: %d\n", i, int_array[i]);
+	}
+	return 0;
 }
 
 static void hello_exit(void)
 {
-    printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye %s\n", whom);
 }
 
 module_init(hello_init);
